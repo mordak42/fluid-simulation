358a54c89c35619a076f618cc0ca760bc1ee6eac
vcombey <vcombey@student.42.fr>
2017-11-28 22:05:42 +0100

    lagrange

--- srcs/utils/main.cpp
+++ srcs/utils/main.cpp
@@ -4,17 +4,38 @@
 
 using namespace mod1;
 
+Polynom lagrange(struct point points[], int nb_point);
+
+void    debug_poly(Polynom poly)
+{
+    for (int i = poly.m_nb_coefs - 1; i >= 0; i--)
+    {
+        std::cout << ' ' << poly.m_coefs[i];
+        std::cout << "*X^" << i << " + ";
+    }
+    std::cout << '\n';
+}
+
 int main ()
 {
-	Polynom poly((double[]){16,2,77,29, 12,23}, 6);
-	Polynom poly_2((double[]){16,2,77,29, 12,23}, 6);
 
-	Polynom poly_3 = poly + poly_2;
-	for (int i = 0; i < poly_3.m_nb_coefs; i++)
-		std::cout << ' ' << poly_3.m_coefs[i];
-	std::cout << '\n';
-	
+    struct point points[3];
 
-//	std::cout<< "six 1 is equal to" << six->eval(1);
-	return 0;
+    points[0].x = 1;
+    points[0].y = 3;
+    points[1].x = -1;
+    points[1].y = 2;
+    points[2].x = 2;
+    points[2].y = -1;
+
+    Polynom poly((double[]){0, 1}, 2); // x
+    Polynom poly_2((double[]){0, 1}, 2); // x + 1
+
+    //Polynom poly_3 = poly * poly_2;
+    Polynom poly_3 = lagrange(points, 3);
+    debug_poly(poly_3);
+
+
+    //std::cout<< "six 1 is equal to" << six->eval(1);
+    return 0;
 }
--- srcs/utils/polynom.hpp
+++ srcs/utils/polynom.hpp
@@ -5,39 +5,77 @@
 #include <iostream>
 #define NB_COEFS_MAX 100
 
-namespace mod1
-{
-class Polynom {
-	public:
-		Polynom(double *coefs, int size);
-		~Polynom();
-		double	eval(double x);
-		double	m_coefs[NB_COEFS_MAX];
-		int		m_nb_coefs;
-		/*
-		** les coeficiant sont stockes dans l ordre 
-		** P(X) = coefs[0] + coefs[1]X + coef[2]X^2 ...
-		*/
-
-friend Polynom operator+ (Polynom &p, Polynom &q)
-{
-	Polynom	res((double[]){0}, 1);
-	int i = 0;
-
-	res.m_nb_coefs = p.m_nb_coefs > q.m_nb_coefs ? p.m_nb_coefs : q.m_nb_coefs;
-	for (; i < p.m_nb_coefs && i < q.m_nb_coefs; i++)
-		res.m_coefs[i] = p.m_coefs[i] + q.m_coefs[i];
-	for (; i < p.m_nb_coefs; i++)
-		res.m_coefs[i] = p.m_coefs[i];
-	for (; i < q.m_nb_coefs; i++)
-		res.m_coefs[i] = q.m_coefs[i];
-	return res;
-}
-
+struct point {
+    double x;
+    double y;
 };
 
-Polynom	operator + (Polynom &p, Polynom &q);
-}
+namespace mod1
+{
+    class Polynom {
+        public:
+            Polynom(double *coefs, int size);
+            ~Polynom();
+            double  eval(double x);
+            double  m_coefs[NB_COEFS_MAX] = {0};
+            int     m_nb_coefs;
+            /*
+             ** les coeficiant sont stockes dans l ordre 
+             ** P(X) = coefs[0] + coefs[1]X + coef[2]X^2 ...
+             */
 
+            friend Polynom operator+ (Polynom &p, Polynom &q)
+            {
+                Polynom res((double[]){0}, 1);
+                int i = 0;
+
+                res.m_nb_coefs = p.m_nb_coefs > q.m_nb_coefs ? p.m_nb_coefs : q.m_nb_coefs;
+                for (; i < p.m_nb_coefs && i < q.m_nb_coefs; i++)
+                    res.m_coefs[i] = p.m_coefs[i] + q.m_coefs[i];
+                for (; i < p.m_nb_coefs; i++)
+                    res.m_coefs[i] = p.m_coefs[i];
+                for (; i < q.m_nb_coefs; i++)
+                    res.m_coefs[i] = q.m_coefs[i];
+                return res;
+            }
+
+            friend Polynom operator/ (Polynom &p, double divisor)
+            {
+                Polynom res((double*)p.m_coefs, p.m_nb_coefs);
+
+                for (int i = 0; i < p.m_nb_coefs; i++)
+                    res.m_coefs[i] = p.m_coefs[i] / divisor;
+                return res;
+            }
+
+            friend Polynom operator* (Polynom &p, double factor)
+            {
+                Polynom res((double*)p.m_coefs, p.m_nb_coefs);
+
+                for (int i = 0; i < p.m_nb_coefs; i++)
+                    res.m_coefs[i] = p.m_coefs[i] * factor;
+                return res;
+            }
+
+            /*
+             ** P(X) * Q(X)[i] = ∑(k = 0; k <=i) p[k]q[i-k]
+             */
+
+            friend Polynom operator* (Polynom &p, Polynom &q)
+            {
+                Polynom res((double[]){0}, 1);
+
+                res.m_nb_coefs = p.m_nb_coefs + q.m_nb_coefs - 1;
+
+                for (int i = 0; i < res.m_nb_coefs; i++) {
+                    res.m_coefs[i] = 0;
+                    for (int k = 0; k <= i; k++)
+                        res.m_coefs[i] += p.m_coefs[k] * q.m_coefs[i - k];
+                }
+                return res;
+            }
+    };
+
+}
 
 #endif
