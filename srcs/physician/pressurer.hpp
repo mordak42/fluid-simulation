3ddd985069a25505faf32604baf9591af777cc8e
vcombey <vcombey@student.42.fr>
2017-12-08 19:37:10 +0100

    almost done

--- srcs/physician/pressurer.hpp
+++ srcs/physician/pressurer.hpp
@@ -28,9 +28,13 @@ private:
 	void calcNegativeDivergence(void);
 	void bzeroVect(double (&vect)[GRID_WIDTH][GRID_HEIGHT]);
 	void calcA();
-	void applyPrecon(double (&r)[GRID_WIDTH][GRID_HEIGHT]);
+	void applyPrecon(double (&r)[GRID_WIDTH][GRID_HEIGHT], double (&res)[GRID_WIDTH][GRID_HEIGHT]);
+	double dotProduct(double (&a)[GRID_WIDTH][GRID_HEIGHT], double (&b)[GRID_WIDTH][GRID_HEIGHT]);
+	double normeVect(double (&vect)[GRID_WIDTH][GRID_HEIGHT]);
 	void calcPrecon();
-	void ApplyA(double (&s)[GRID_WIDTH][GRID_HEIGHT], double (&res)[GRID_WIDTH][GRID_HEIGHT]);
+	void PCG(void);
+	void cpyVect(double (&original)[GRID_WIDTH][GRID_HEIGHT], double (&res)[GRID_WIDTH][GRID_HEIGHT]);
+	void applyA(double (&s)[GRID_WIDTH][GRID_HEIGHT], double (&res)[GRID_WIDTH][GRID_HEIGHT]);
     const std::shared_ptr<PhysicItems> &m_physicItems;
 	struct Acell   A[GRID_WIDTH][GRID_HEIGHT];
 	double	       precon[GRID_WIDTH][GRID_HEIGHT];
@@ -38,6 +42,7 @@ private:
     double	       s[GRID_WIDTH][GRID_HEIGHT];
     double	       b[GRID_WIDTH][GRID_HEIGHT];
     double	       p[GRID_WIDTH][GRID_HEIGHT];
+    double	       r[GRID_WIDTH][GRID_HEIGHT];
 
 	/* for the applyPrecon */
     double	       q[GRID_WIDTH][GRID_HEIGHT];
